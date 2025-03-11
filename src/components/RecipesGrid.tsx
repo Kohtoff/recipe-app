fa248fbe937fdf09f5fd3b2a3c635b3b704f3ac9
Kohtoff <danil.kohtoff@gmail.com>
2025-03-12 00:05:12 +0100

    reduce gaps on home page

--- src/components/RecipesGrid.tsx
+++ src/components/RecipesGrid.tsx
@@ -15,7 +15,7 @@ const RecipeGrid = ({ recipes }: Props) => {
   }
 
   return (
-    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+    <div className="my-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       {recipes.map((recipe) => (
         <RecipeCard key={recipe.idMeal} recipe={recipe} />
       ))}
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -5,7 +5,7 @@ const AppLayout = () => {
   return (
     <div className="min-h-screen min-w-screen relative flex flex-col">
       <Header />
-      <main className="mx-auto w-full grow flex flex-col bg-white max-w-2xl px-4 py-6 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
+      <main className="mx-auto w-full grow flex flex-col bg-white max-w-2xl px-4 py-6 sm:px-6 lg:max-w-7xl lg:px-8">
         {<Outlet />}
       </main>
     </div>
